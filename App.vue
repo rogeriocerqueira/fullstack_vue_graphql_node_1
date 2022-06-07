0f303bda911a6165486b7f1d47a9a8c26ba0f135
Rogério Cerqueira <rogeriocerqueira.uefs@gmail.com>
2022-06-07 13:46:29 -0300

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,91 @@
+<template>
+<div>
+    <div id = "slogan"> 
+      <div class = "text-center"> 
+        <h1> NameGator </h1>
+        <br/>
+        <h6 class = "text-secondary"> Gerador de nomes utilizando o Vue.js</h6>
+      </div>
+    </div>
+    <div id = "main">
+      <div class="container">
+        <div class="row"> 
+        <div class = "col-md">
+          <h5>Prefixos <span class = "badge badge-info"> {{ prefixes.lengh }} </span></h5>
+           <div class = "card">
+             <div class="card-body"> 
+               <ul class = "list-group">
+                 <li class = "list-group-item" v-for = "prefix in prefixes" v-bind:key="prefix"> 
+                      {{ prefix }}
+                   </li>
+               </ul>
+               <br/>
+               <input class = "form-control" type = "text" placeholder="Digite o prefixo"/> 
+             </div>
+
+           </div>
+        </div>
+        <div class = "col-md">
+          <h5>Sufixos <span class = "badge badge-info"> {{sufixes.lengh}}</span></h5>
+          <div class = "card">
+             <div class="card-body"> 
+               <ul class = "list-group">
+                 <li class = "list-group-item" v-for = " sufix in sufixes" v-bind:key="sufix">
+                      {{ sufix }}
+                   </li>
+               </ul>
+               <br/>
+               <input class = "form-control" type = "text" placeholder="Digite o sufixo"/> 
+             </div>             
+           </div>
+        </div>
+        <div>
+        </div>
+        </div>
+        <br/>
+        <h5>Domínios<span class = "badge badge-info">{{domains.lengh}}</span></h5>
+        <div class ="card"> 
+          <div class = "card-body">
+            <ul class = "list-group">
+              <li class = "list-group-item" v-for="domain in domains" v-bind:key="domain">
+                  {{domain}}
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+    </div>
+</div>
+</template>
+
+<script>
+import "bootstrap/dist/css/bootstrap.css"
+import "font-awesome/css/font-awesome.css"
+
+export default {
+  name: 'App',
+  data: function() {
+    return {
+      prefixes: ['Air', 'Jet', 'Flight'],
+      sufixes: ['Hub', 'Station', 'Mart'],
+      domains: ['Airhub', 'AirStation', 'AirMart', 'JetHub', 'JetStation', 'JetMart', 'FlightHub', 'FlightStation', 'FlightMart' ]
+    };
+  }
+
+  }
+
+</script>
+<style>
+  #slogan {
+    margin: top 30px;
+    margin-bottom: 30px;
+  }
+  
+  #main {
+    background-color: #F1F1F1;
+    padding-top: 30px;
+  }
+
+
+</style>
