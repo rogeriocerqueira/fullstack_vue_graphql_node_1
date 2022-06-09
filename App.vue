a3f250078d9f97e2da03682a8924acac5c9c84db
Rogério Cerqueira <rogeriocerqueira.uefs@gmail.com>
2022-06-09 17:12:26 -0300

    alterações

--- App.vue
+++ App.vue
@@ -1,121 +1,53 @@
 <template>
-<div>
-    <div id = "slogan"> 
-      <div class = "text-center"> 
-        <h1> NameGator </h1>
-        <br/>
-        <h6 class = "text-secondary"> Gerador de nomes utilizando o Vue.js</h6>
-      </div>
-    </div>
-    <div id = "main">
-      <div class="container">
-        <div class="row"> 
-        <div class = "col-md">
-          <h5>Prefixos <span class = "badge badge-info"> {{ prefixes.lengh }} </span></h5>
-           <div class = "card">
-             <div class="card-body"> 
-               <ul class = "list-group">
-                 <li class = "list-group-item" v-for = "prefix in prefixes" v-bind:key="prefix"> 
-                      {{ prefix }}
-                   </li>
-               </ul>
-               <br/>
-               <div class = "input-group">
-               <input class = "form-control" type = "text" v-model="prefix" placeholder="Digite o prefixo"/>
-               <div class="input-group-controle">
-                 <button class="btn btn-info" v-on:click="addPrefix(prefix)"><span class="fa fa-plus"></span></button>
-               </div>
-             </div>
-             </div>
-           </div>
-        </div>
-        <div class = "col-md">
-          <h5>Sufixos <span class = "badge badge-info"> {{sufixes.lengh}}</span></h5>
-          <div class = "card">
-             <div class="card-body"> 
-               <ul class = "list-group">
-                 <li class = "list-group-item" v-for = "sufix in sufixes" v-bind:key="sufix">
-                      {{ sufix }}
-                   </li>
-               </ul>
-               <br/>
-               <div class = "input-group">
-               <input class = "form-control" type = "text" v-model="sufix" placeholder="Digite o Sufixo"/>
-               <div class="input-group-controle">
-                <button class="btn btn-info" v-on:click="addSufix(sufix)"> <span class="fa fa-plus"> </span></button>
-               </div>
-             </div>
-             </div>             
-           </div>
-        </div>
-        <div>
-        </div>
-        </div>
-        <br/>
-        <h5>Domínios<span class = "badge badge-info">{{domains.lengh}}</span></h5>
-        <div class ="card"> 
-          <div class = "card-body">
-            <ul class = "list-group">
-              <li class = "list-group-item" v-for="domain in domains" v-bind:key="domain">
-                  {{ domain }}
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-
-    </div>
-</div>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      For a guide and recipes on how to configure / customize this project,<br>
+      check out the
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+    </p>
+    <h3>Installed CLI Plugins</h3>
+    <ul>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+    </ul>
+    <h3>Essential Links</h3>
+    <ul>
+      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
+      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
+      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
+      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
+      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
+    </ul>
+    <h3>Ecosystem</h3>
+    <ul>
+      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
+      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
+      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
+      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
+      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+    </ul>
+  </div>
 </template>
 
 <script>
-import "bootstrap/dist/css/bootstrap.css";
-import "font-awesome/css/font-awesome.css";
-
-export default {
-	name: "App",
-	data: function() {
-		return {
-			prefix: "",
-			sufix: "",
-			prefixes: ["Air", "Jet", "Flight"], 
-			sufixes: ["Hub", "Station", "Mart"],
-			domains: ["Airhub", "AirStation", "AirMart", "JetHub", "JetStation", "JetMart", "FlightHub", "FlightStation", "FlightMart" ]}
-		;},
-    
-	methods: { 
-		addPrefix(prefix){ 
-			this.prefixes.push(prefix);
-			this.prefix = "";
-			this.generate();
-		}, 
-		addSufix(sufix){ this.sufixes.push(sufix);
-			this.sufix = "";
-			this.generate();
-		}
-	},
-
-	generate() {
-		this.domains = [];
-		for(const prefix of this.prefixes){ 
-			for(const sufix of this.sufixes){
-				this.domains.push(prefix + sufix);
-			}
-		}
-	}
-};
-
+export default { name: "HelloWorld", props: { msg: String } };
 </script>
-<style>
-  #slogan {
-    margin: top 30px;
-    margin-bottom: 30px;
-  }
-  
-  #main {
-    background-color: #F1F1F1;
-    padding-top: 30px;
-  }
 
-
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
 </style>
